7079009a1061173a2cab6c6eace690f4a7d9c45e
Menno Veerkamp <veerkampmenno@gmail.com>
2024-07-19 17:24:53 +0200

    fix(scripts): add IIFE for already declared variables

--- scripts/content.js
+++ scripts/content.js
@@ -1,25 +1,27 @@
-const html = document.documentElement;
+(() => {
+  const html = document.documentElement;
 
-const setFilters = async () => {
-  const result = await chrome.storage.local.get("filters")
-  const filters = JSON.parse(result.filters);
-  
-  const { sepia, saturate, opacity, invert, huerotate, grayscale, contrast, brightness, blur } = filters;
+  const setFilters = async () => {
+    const result = await chrome.storage.local.get("filters")
+    const filters = JSON.parse(result.filters);
+    
+    const { sepia, saturate, opacity, invert, huerotate, grayscale, contrast, brightness, blur } = filters;
 
-  const filter = `
-    sepia(${sepia / 100})
-    saturate(${saturate / 100 })
-    opacity(${opacity / 100})
-    invert(${invert / 100})
-    hue-rotate(${huerotate}deg)
-    grayscale(${grayscale / 100})
-    contrast(${contrast / 100})
-    brightness(${brightness / 100})
-    blur(${blur}px)
-  `
+    const filter = `
+      sepia(${sepia / 100})
+      saturate(${saturate / 100 })
+      opacity(${opacity / 100})
+      invert(${invert / 100})
+      hue-rotate(${huerotate}deg)
+      grayscale(${grayscale / 100})
+      contrast(${contrast / 100})
+      brightness(${brightness / 100})
+      blur(${blur}px)
+    `
 
-  html.style.setProperty("filter", filter, "important");
-  chrome.storage.local.set(filters);
-}
+    html.style.setProperty("filter", filter, "important");
+    chrome.storage.local.set(filters);
+  }
 
-setFilters()
+  setFilters()
+})()
